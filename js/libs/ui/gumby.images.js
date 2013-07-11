7cf85a242c3b0b067325128d0900ea7f5fcdbb8f
Adam Chambers <adammchambers@gmail.com>
2013-07-10 20:59:04 -0400

    Commented up and tidied images module

--- js/libs/ui/gumby.images.js
+++ js/libs/ui/gumby.images.js
@@ -5,28 +5,32 @@
 
 	'use strict';
 
-	if(!Modernizr) {
-		return false;
-	}
-
 	function Images($el) {
 
 		this.$el = $el;
+		// supports attribute in format test:image
 		this.supports = Gumby.selectAttr.apply(this.$el, ['supports']) || false;
-		this.def = Gumby.selectAttr.apply(this.$el, ['default']) || false;
+		// media attribute in format mediaQuery:image
 		this.media = Gumby.selectAttr.apply(this.$el, ['media']) || false;
+		// default image to load
+		this.def = Gumby.selectAttr.apply(this.$el, ['default']) || false;
 
-		if(this.supports) {
+		// if support attribute supplied and Modernizr is present
+		if(this.supports && Modernizr) {
+			// parse and handle that attribute
 			this.supports = this.parseSupport(this.supports);
 			this.handleSupports(this.supports);
 		}
 	}
 
+	// handle supports object checking each prop for support
 	Images.prototype.handleSupports = function() {
 		var scope = this,
 			supported = false;
 
 		$(this.supports).each(function(key, val) {
+			// modernizr test passes so load in image 
+			// supplied in order of preference so halt each loop
 			if(!!Modernizr[val.test]) {
 				scope.insertImage(val.img);
 				supported = true;
@@ -34,11 +38,13 @@
 			}
 		});
 
+		// no feature supported so load default if supplied
 		if(!supported && this.def) {
 			this.insertImage(this.def);
 		}
 	};
 
+	// preload image and update image src
 	Images.prototype.insertImage = function(img) {
 		var scope = this,
 			image = $(new Image());
@@ -48,16 +54,19 @@
 		}).attr('src', img);
 	};
 
+	// parse support attribute string
 	Images.prototype.parseSupport = function(support) {
 		var supp = support.split(','),
 			res = [], splt = [];
 
+		// multiple can be supplied so loop round and create object 
 		$(supp).each(function(key, val) {
 			splt = val.split(':');
 			if(splt.length !== 2) {
 				return true;
 			}
 
+			// object containing Modernizr test and image url
 			res.push({
 				'test' : splt[0],
 				'img' : splt[1]
