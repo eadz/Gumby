355cc71944040dc8e3193f65236db3f9dc03880c
Adam Chambers <adammchambers@gmail.com>
2013-07-26 16:05:15 -0400

    Cleaned up basic working example of DOM shuffling module

--- js/libs/ui/gumby.shuffle.js
+++ js/libs/ui/gumby.shuffle.js
@@ -9,20 +9,26 @@
 
 		this.$el = $el;
 
+		// array of children
 		this.children = $.makeArray(this.$el.children('.columns,.column'));
+		// parse shuffle attribute into array of test:sequence objects
 		this.shuffles = this.parseAttrValue(Gumby.selectAttr.apply(this.$el, ['shuffle']));
 		this.shuffled = false;
 
 		var scope = this;
 
+		// handle tests now and then on resize
 		this.handleTests();
 		$(window).on('resize', function() {
 			scope.handleTests();
 		});
 	}
 
+	// loop round each test
+	// if matchMedia passes then shuffle with that sequence
 	Shuffle.prototype.handleTests = function() {
 		var scope = this, succes = false;
+
 		$(this.shuffles).each(function(key, val) {
 			if(window.matchMedia(val.test).matches) {
 				scope.shuffle(val.sequence);
@@ -30,33 +36,40 @@
 			}
 		});
 
+		// no tests passed so use original order
+		// no arg passed for this
 		if(!succes) {
 			this.shuffle();
 		}
 	};
 
+	// shuffle children into supplied sequence
+	// sequence in format 1-2-3-4-5
 	Shuffle.prototype.shuffle = function(sequence) {
-		console.log("SHUFFLING...");
 		var scope = this,
 			seq = [],
 			newArr = [];
 
+		// if sequence passed then fill newArr up with reordered children
 		if(sequence) {
 			seq = sequence.split('-');
 			$(seq).each(function(index) {
-				newArr.push($(scope.children[seq[index]]));
+				newArr.push($(scope.children[Number(seq[index])]));
 			});
+
+		// otherwise newArr is just the children array
 		} else {
 			newArr = this.children;
 		}
 
+		// empty parent and loop round newArr appending each child
 		this.$el.html('');
-
 		$(newArr).each(function() {
 			scope.$el.append($(this));
 		});
 	};
 
+	// return array of test:sequence objects
 	Shuffle.prototype.parseAttrValue = function(str) {
 		var supp = str.split(','),
 			res = [], splt = [];
@@ -68,7 +81,7 @@
 				return true;
 			}
 
-			// object containing Modernizr test or media query and image url
+			// object containing Modernizr test or media query and dash separated sequence
 			res.push({
 				'test' : splt[0],
 				'sequence' : splt[1]
